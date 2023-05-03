43d4c3d13930366cc1b72f9fee5ec7e50d819254
nathanie.assan <nathanie.assan@turntabl.io>
2023-05-03 23:28:07 +0000

    koinovotemanager

--- server.js
+++ server.js
@@ -51,6 +51,6 @@ app.use((error, req, res, next) => {
 let myName = 0;
 
 // LISTEN FOR ROUTES
-app.listen(process.env.$PORT || "3032", (req, res) => {
+app.listen(process.env.PORT || "3032", (req, res) => {
   console.log("now listening on port", "3032,", req);
 });
